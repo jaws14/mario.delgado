14b41d5b9071b8e0df65c1c1303202fb0028c73e
pablo j delgado <fluffydemonkey@gmail>
2014-12-01 15:01:06 -0800

    added code

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -29,6 +29,6 @@ game.PlayScreen = me.ScreenObject.extend({
         
         resetPlayer: function(x, y  ){
            var player = me.pool.pull("mario", x, y, {});
-           me.game.world.addChild(player, 6); 
+           me.game.world.addChild(player, 5); 
         }
 });
